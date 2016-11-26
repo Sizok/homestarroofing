19814d8548626e77ddeb31ecd70513b1ce32edb9
Sizok <mrsizok3@gmail.com>
2016-11-26 13:46:00 +0200

    make new font

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+	$(window).scroll(function () {
+		var st = $(this).scrollTop();
+		if (st<2300) {
+			$('#hero-banner').css({
+				'transform': 'translate(0%, ' + st / 6 + '%'
+			});
+			$('.main-text').css({
+				'transform': 'translate(0%, -' + st / 1.5 + '%'
+			});
+		}
+	});
 	$(function() {
 		$(".rslides").responsiveSlides();
 	});
